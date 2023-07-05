bb998bce9cfdd8e876930317411b05dddca100a0
naravit bunthap <whichy@naravits-MacBook-Air.local>
2023-07-05 19:44:17 +0700

    deploy homework

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import viteLogo from '../public/vit.jpg'
+// import photo from 
+// const photo  = require('./image/my_photo_copy.PNG')
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src={viteLogo} style={{width:"300px"}} />
+      <h1 style={{textAlign:"center"}}>Wichy</h1>
+    </>
+  )
+}
+
+export default App
